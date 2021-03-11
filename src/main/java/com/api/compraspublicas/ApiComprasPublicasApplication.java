cb380a80390528e355c20ded674b84c65e3c24c8
Kennedy Oliveira Rocha <kennedy279972@gmail.com>
2021-03-11 13:34:31 -0400

    Projeto Iniciado

---
+++ src/main/java/com/api/compraspublicas/ApiComprasPublicasApplication.java
@@ -0,0 +1,13 @@
+package com.api.compraspublicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComprasPublicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComprasPublicasApplication.class, args);
+	}
+
+}
