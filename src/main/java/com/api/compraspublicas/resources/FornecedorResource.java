2b71cb2cad3279828b335018361f49b63587af87
kennedyrocha <kennedy279972@gmailcom>
2021-03-13 16:29:16 -0400

    Criado classe fornecedor

--- src/main/java/com/api/compraspublicas/resources/FornecedorResource.java
+++ src/main/java/com/api/compraspublicas/resources/FornecedorResource.java
@@ -1,15 +1,28 @@
 package com.api.compraspublicas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.compraspublicas.domain.*;
 
 @RestController
 @RequestMapping(value="/fornecedores")
 public class FornecedorResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Fornecedor> listar() {
+		
+		Fornecedor for1 = new Fornecedor(1, "Teste 1");
+		Fornecedor for2 = new Fornecedor(2, "Teste 2");
+		
+		List<Fornecedor> lista = new ArrayList<>();
+		lista.add(for1);
+		lista.add(for2);
+		
+		return lista;
 	}
 }
