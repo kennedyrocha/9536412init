125456fb09cd27f9abc6a73ce6cbcedca3d7440a
Kennedy Oliveira Rocha <kennedy279972@gmail.com>
2021-03-15 12:47:15 -0400

    Repository and service for Fornecedor

---
+++ src/main/java/com/api/compraspublicas/repositories/FornecedorRepository.java
@@ -0,0 +1,11 @@
+package com.api.compraspublicas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.compraspublicas.domain.Fornecedor;
+
+@Repository
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
+
+}
---
+++ src/main/java/com/api/compraspublicas/services/FornecedorService.java
@@ -0,0 +1,21 @@
+package com.api.compraspublicas.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.compraspublicas.domain.Fornecedor;
+import com.api.compraspublicas.repositories.FornecedorRepository;
+
+@Service
+public class FornecedorService {
+
+	@Autowired
+	private FornecedorRepository repo;
+	
+	public List<Fornecedor> findAll(){
+		
+		return repo.findAll();
+	}
+}
